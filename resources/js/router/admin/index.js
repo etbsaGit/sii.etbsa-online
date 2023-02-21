9143150883190494b92aac8dadf3cfcd0d2f82d0
Abraham Martinez <mga.abraham@gmail.com>
2023-02-21 16:35:27 -0600

    Refactor folders AdminJs  Router

---
+++ resources/js/router/admin/index.js
@@ -0,0 +1,23 @@
+import { sales } from "~/router/admin/sales";
+import { users } from "~/router/admin/users";
+import { gps } from "~/router/admin/gps";
+import { vehicles } from "~/router/admin/vehicles";
+import { purchases } from "~/router/admin/purchases";
+import { system } from "~/router/admin/system";
+import { rrhh } from "~/router/admin/rrhh";
+import { customers } from "~/router/admin/customers";
+import { nt } from "~/router/admin/nt";
+import { products } from "~/router/admin/products";
+
+export const admin = [
+  ...sales,
+  ...users,
+  ...gps,
+  ...vehicles,
+  ...purchases,
+  ...system,
+  ...rrhh,
+  ...customers,
+  ...nt,
+  ...products
+];
---
+++ resources/js/router/admin/purchases.js
@@ -0,0 +1,50 @@
+export const purchases = [
+  {
+    path: "/purchases",
+    component: require("@admin/purchases/Main").default,
+    children: [
+      {
+        path: "list",
+        name: "purchase.list",
+        component: require("@admin/purchases/orders/Index").default,
+      },
+      {
+        path: "create",
+        name: "purchase.create",
+        component: require("@admin/purchases/orders/Create").default,
+      },
+      {
+        path: ":purchaseId/edit",
+        name: "purchase.edit",
+        component: require("@admin/purchases/orders/Edit").default,
+        props: true,
+      },
+      {
+        path: "suppliers",
+        name: "suppliers.list",
+        component: require("@admin/purchases/suppliers/Index").default,
+      },
+      {
+        path: "suppliers/create",
+        name: "suppliers.create",
+        component: require("@admin/purchases/suppliers/Create").default,
+      },
+      {
+        path: "suppliers/:supplierId/edit",
+        name: "suppliers.edit",
+        component: require("@admin/purchases/suppliers/Edit").default,
+        props: true,
+      },
+      {
+        path: "purchase-invoices",
+        name: "purchase.invoice.list",
+        component: require("@admin/purchases/invoices/Index").default,
+      },
+      {
+        path: "purchase-concepts",
+        name: "purchase.concepts.index",
+        component: require("@admin/purchases/concepts/Index").default,
+      },
+    ],
+  },
+];
