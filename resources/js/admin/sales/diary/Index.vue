3517b8c321c4195a5f3b93cbc48dc97b0ee21465
AbrahamMZ <mga.abraham@gmail.com>
2023-04-19 11:13:45 -0600

    New Filter Seller in Calendar Tracking

--- resources/js/admin/sales/diary/Index.vue
+++ resources/js/admin/sales/diary/Index.vue
@@ -1,30 +1,39 @@
 <template>
   <v-card height="100%">
     <v-navigation-drawer v-model="drawer" width="250" touchless absolute>
-      <v-sheet
-        color="grey lighten-4"
-        class="d-flex pa-4 justify-center align-center"
-      >
-        <v-btn color="primary">Agregar Actividad</v-btn>
+      <v-sheet color="grey lighten-4" class="d-flex pa-4 align-center">
+        <v-subheader class="text-h5">Filtro</v-subheader>
       </v-sheet>
 
       <v-divider></v-divider>
+
+      <v-row class="pa-2">
+        <v-col cols="12">
+          <v-autocomplete
+            v-model="filters.sellers"
+            :items="options.sellers"
+            item-text="name"
+            item-value="id"
+            label="Filtro Vendedor:"
+            hide-details
+            clearable
+            multiple
+            filled
+          ></v-autocomplete>
+        </v-col>
+      </v-row>
     </v-navigation-drawer>
     <v-card :style="`margin-left: ${drawer ? '250px;' : ''} `" flat>
       <v-app-bar dense flat color="white">
-        <!-- <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon> -->
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
         <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
           HOY
         </v-btn>
         <v-btn fab text small color="grey darken-2" @click="prev">
-          <v-icon small>
-            mdi-chevron-left
-          </v-icon>
+          <v-icon small> mdi-chevron-left </v-icon>
         </v-btn>
         <v-btn fab text small color="grey darken-2" @click="next">
-          <v-icon small>
-            mdi-chevron-right
-          </v-icon>
+          <v-icon small> mdi-chevron-right </v-icon>
         </v-btn>
         <v-toolbar-title v-if="$refs.calendar" class="overline">
           {{ $refs.calendar.title }}
@@ -34,9 +43,7 @@
           <template v-slot:activator="{ on, attrs }">
             <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
               <span>{{ typeToLabel[type] }}</span>
-              <v-icon right>
-                mdi-menu-down
-              </v-icon>
+              <v-icon right> mdi-menu-down </v-icon>
             </v-btn>
           </template>
           <v-list>
@@ -61,7 +68,7 @@
           :type="type"
           @click:event="showEvent"
           @click:more="viewDay"
-          style="height: 80vh;"
+          style="height: 80vh"
         ></v-calendar>
         <v-menu
           v-model="selectedOpen"
@@ -116,7 +123,7 @@
 export default {
   data() {
     return {
-      drawer: false,
+      drawer: true,
       right: null,
       focus: "",
       type: "month",
@@ -129,6 +136,12 @@ export default {
       selectedElement: null,
       selectedOpen: false,
       events: [],
+      filters: {
+        sellers: [],
+      },
+      options: {
+        sellers: [],
+      },
     };
   },
 
@@ -138,8 +151,17 @@ export default {
       { label: "Seguimientos", to: { name: "tracking.list" } },
       { label: "Calendario", to: { name: "" } },
     ]);
+    this.loadOptions();
     this.loadHistorical();
     // this.updateRange({ start, end });
+  },
+  watch: {
+    filters: {
+      handler: _.debounce(function (v) {
+        this.loadHistorical(() => {});
+      }, 700),
+      deep: true,
+    },
   },
   methods: {
     viewDay({ date }) {
@@ -180,10 +202,26 @@ export default {
     async loadHistorical(cb) {
       const _this = this;
 
+      let payload = {
+        sellers: _this.filters.sellers.join(","),
+      };
+
       await axios
-        .get("/admin/tracking/historical/diary")
+        .get("/admin/tracking/historical/diary", { params: payload })
         .then(function (response) {
           _this.events = response.data.data.event;
+          (cb || Function)();
+        });
+    },
+    async loadOptions(cb) {
+      const _this = this;
+      let params = {
+        paginate: "no",
+      };
+      await axios
+        .get("/admin/sellers", { params: params })
+        .then(function (response) {
+          _this.options.sellers = response.data.data;
           (cb || Function)();
         });
     },
