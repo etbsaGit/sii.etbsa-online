d5507a137349dd67525b6a218bda292089543597
AbrahamMZ <mga.abraham@gmail.com>
2023-11-29 08:42:32 -0600

    Add new Menu in Customer Reports History

--- resources/js/admin/customers/reports/MapsTabs.vue
+++ resources/js/admin/customers/reports/MapsTabs.vue
@@ -1,13 +1,13 @@
 <template>
   <v-card color="basil">
     <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
-      <v-tab v-for="item in items" :key="item">
+      <v-tab v-for="(item, index) in items" :key="`tab_mapa_${index}`">
         {{ item.name }}
       </v-tab>
     </v-tabs>
 
     <v-tabs-items v-model="tab">
-      <v-tab-item v-for="item in items" :key="item">
+      <v-tab-item v-for="(item, index) in items" :key="`mapa_${index}`">
         <v-card color="basil" flat>
           <iframe
             :src="item.src"
--- resources/js/router/admin/customers.js
+++ resources/js/router/admin/customers.js
@@ -25,9 +25,19 @@ export const customers = [
         component: require("@admin/customers/reports/DuePortFolio").default,
       },
       {
+        path: "customers/last-invoice",
+        name: "customers.last-invoice",
+        component: require("@admin/customers/customers/Index").default,
+      },
+      {
         path: "reports/info-geografica",
         name: "reports.geografica",
         component: require("@admin/customers/reports/MapsTabs").default,
+      },
+      {
+        path: "reports/adr",
+        name: "reports.adr",
+        component: require("@admin/customers/reports/MapsADRTabs").default,
       },
     ],
   },
