472884e4e2c6f8fc97110b6ea27c3eb6b29cc58c
AbrahamMZ <mga.abraham@gmail.com>
2024-01-22 09:35:43 -0600

    Init New Menu Flujo Efectivo

---
+++ resources/js/admin/FlujoEfectivo/PolizaEgresos/Index.vue
@@ -0,0 +1,8 @@
+<template >
+    <div>Polizas de Egresos</div>
+  </template>
+  <script>
+  export default {};
+  </script>
+  <style >
+  </style>
---
+++ resources/js/admin/FlujoEfectivo/PolizaIngresos/Index.vue
@@ -0,0 +1,8 @@
+<template >
+  <div>Polizas de Ingresos</div>
+</template>
+<script>
+export default {};
+</script>
+<style >
+</style>
