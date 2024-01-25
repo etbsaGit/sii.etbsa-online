530ed2713d6dc01defc0f79e4f78a1858abb1f6f
AbrahamMZ <mga.abraham@gmail.com>
2024-01-25 16:25:14 -0600

    Init BankPolicy CRUD routes

--- routes/web/common.php
+++ routes/web/common.php
@@ -4,5 +4,6 @@ Route::prefix('admin')->namespace('Admin')->middleware(['auth'])->group(function
 
     // resource
     Route::resource('agency-bank-accounts', 'AgencyBankAccountController');
+    Route::resource('bank-policy', 'BankPolicyController');
 
 });
