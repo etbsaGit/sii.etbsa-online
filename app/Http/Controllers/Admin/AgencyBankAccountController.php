49e4ddd4b11298e8679055bef770562b1271a8d5
AbrahamMZ <mga.abraham@gmail.com>
2024-01-25 10:53:02 -0600

    init Model AgencyBankAccounts

---
+++ app/Http/Controllers/Admin/AgencyBankAccountController.php
@@ -0,0 +1,86 @@
+<?php
+
+
+namespace App\Http\Controllers\Admin;
+
+use App\Components\Common\Models\AgencyBankAccount;
+use Illuminate\Http\Request;
+
+class AgencyBankAccountController extends AdminController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Components\Common\Models\AgencyBankAccount  $agencyBankAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AgencyBankAccount $agencyBankAccount)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Components\Common\Models\AgencyBankAccount  $agencyBankAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AgencyBankAccount $agencyBankAccount)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Components\Common\Models\AgencyBankAccount  $agencyBankAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AgencyBankAccount $agencyBankAccount)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Components\Common\Models\AgencyBankAccount  $agencyBankAccount
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AgencyBankAccount $agencyBankAccount)
+    {
+        //
+    }
+}
