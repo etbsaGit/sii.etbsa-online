3b20e0d2bf2ffeb818813b8b30f7ea9fd73e8a05
AbrahamMZ <mga.abraham@gmail.com>
2024-02-06 09:31:41 -0600

    Add Column getIncome in AgencyBanck Account

--- app/Components/Common/Models/AgencyBankAccount.php
+++ app/Components/Common/Models/AgencyBankAccount.php
@@ -2,11 +2,12 @@
 
 namespace App\Components\Common\Models;
 
+use App\Components\FlujoEfectivo\Models\BankPolicy;
 use Illuminate\Database\Eloquent\Model;
 
 class AgencyBankAccount extends Model
 {
-       /**
+    /**
      * The database table used by the model.
      *
      * @var string
@@ -20,8 +21,24 @@ class AgencyBankAccount extends Model
      */
     protected $fillable = ['agency_id', 'bank_name', 'account_number', 'balance'];
 
+    protected $appends = ['income_balance'];
+
     public function agency()
     {
         return $this->belongsTo(Agency::class, 'agency_id');
+    }
+
+    public function bankPolicy()
+    {
+        return $this->hasMany(BankPolicy::class, 'agency_bank_accounts_id', 'id');
+    }
+
+    public function getIncomeBalanceAttribute()
+    {
+        $total = $this->bankPolicy->reduce(function ($carry, $item) {
+            return $carry + $item->amount;
+        });
+
+        return $total;
     }
 }
