2c5496edb2b84ab799ce7303035afa446a2e301d
AbrahamMZ <mga.abraham@gmail.com>
2024-02-20 12:55:01 -0600

    Modulo Flujo Efectivo FrontEnd

---
+++ resources/js/admin/FlujoEfectivo/PolizaIngresos/Create.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-card>
+    <v-card-title> Registrar Ingreso </v-card-title>
+    <v-card-text>
+      <ingreso-form :editedItem.sync="form"></ingreso-form>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="blue darken-1" block dark @click="save"> Guardar </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+import IngresoForm from "../IngresoForm.vue";
+export default {
+  components: { IngresoForm },
+  props: ["propTipoPoliza"],
+  data: () => {
+    return {
+      form: {
+        // external_id: null,
+        reference_number: null,
+        reference_name: null,
+        reference_concept: null,
+        description: null,
+        amount: 0,
+        currency_id: 1,
+        payment_source_id: 1,
+        tipo_poliza_id: 1,
+        category_id: null,
+        // origen_bank_accounts_id: null,
+        apply_bank_accounts_id: null,
+        is_applied: false,
+        // apply_date: null,
+        // user_created: null,
+        // user_updated: null,
+      },
+    };
+  },
+  methods: {
+    async save() {
+      const _this = this;
+
+      try {
+        await axios.post(`/admin/polizas`, _this.form);
+      } catch (error) {
+        return;
+      }
+    },
+  },
+};
+</script>
+<style>
+</style>
