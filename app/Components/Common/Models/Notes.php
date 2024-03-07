aa21b59ddd9cc5f20d6aef155d04da6ac2f2b512
AbrahamMZ <mga.abraham@gmail.com>
2024-03-07 15:13:46 -0600

    backend Notas en Seguimientos

--- app/Components/Common/Models/Notes.php
+++ app/Components/Common/Models/Notes.php
@@ -2,14 +2,20 @@
 
 namespace App\Components\Common\Models;
 
+use App\Components\User\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Notes extends Model
 {
 
-    protected $fillable = ['body'];
+    protected $fillable = ['body', 'created_by'];
     public function noteable()
     {
         return $this->morphTo();
+    }
+
+    public function createdBy()
+    {
+        return $this->hasOne(User::class, 'id','created_by');
     }
 }
